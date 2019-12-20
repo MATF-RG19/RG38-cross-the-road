403f2bf7ff6881385c33efeca595408716d3349b
AleksandarLisov <sasa.lisov@yahoo.com>
2019-12-20 14:22:33 +0100

    dodata animacija

--- CrossTheRoad/main.c
+++ CrossTheRoad/main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <GL/glut.h>
-#include "main.h"
+#include <time.h>
 
 static float animation_ongoing = 0;
 static float animation_parameter = 0;
 
 const static float size = 0.2; // velicina pileta
+const static float vector = 0.3; // velicina za koju se pile krece
+static char side; // pravac i smer u kom se pomera pile (l || r || f || b)
+static char current_step = 0; // trenutni potez koji je ucinjen - treba nam zbog rotacije pileta kad se krece u stranu
 
 static float x_curr; // x koordinata pileta po ravni z0
 static float y_curr; // y koordinata pileta po ravni z0
-
-const static float vector = 0.3; // velicina za koju se pile krece
-static char side; // pravac i smer u kom se pomera pile (l || r || f || b)
 
 static int points; // trenutni broj poena (uvecava se za svako kretanje unapred)
 
@@ -24,6 +26,8 @@ static void on_keyboard_for_arrows(int key, int x, int y); // osluskivac za stre
 static void initialize(void);
 static void jumpCheck(unsigned char m); // funkcija koja proverava da li je moguce kretanje u zadatom pravcu
 static void checkAndStartTimer(); // funkcija koja pokrece tajmer na prvi dodir nekog od tastera za kretanje
+void drawChicken(); // funkcija za crtanje pileta
+static void rotateChicken(char current_step); // funkcija koja rotira pile kad pri promeni pravca ili smera kretanja
 
 #define TIMER_INTERVAL 20
 #define TIMER_ID 0
@@ -135,6 +139,7 @@ static void jumpCheck(unsigned char m){ ////////////////////////////////////////
             if (x_curr - vector >= -(1.5 - size / 2)){
                 x_curr -= vector;
             }
+            current_step = 'l';
             checkAndStartTimer();
             glutPostRedisplay();
             break;
@@ -142,11 +147,13 @@ static void jumpCheck(unsigned char m){ ////////////////////////////////////////
             if (x_curr + vector <= 1.5 - size / 2){
                 x_curr += vector;
             }
+            current_step = 'r';
             checkAndStartTimer();
             glutPostRedisplay();
             break;
         case 'f':
             y_curr += vector;
+            current_step = 'f';
             checkAndStartTimer();
             glutPostRedisplay();
             points ++;
@@ -158,6 +165,7 @@ static void jumpCheck(unsigned char m){ ////////////////////////////////////////
                 points --;;
                 printf("Points: %d\n", points);
             }
+            current_step = 'b';
             checkAndStartTimer();
             glutPostRedisplay();
             break;
@@ -199,6 +207,96 @@ static void checkAndStartTimer(){
         animation_ongoing = 1;
         glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
     }
+}
+
+void drawChicken(){ // funkcija za crtanje pileta
+    
+    rotateChicken(current_step);
+    
+    glPushMatrix(); // glavni deo 1
+        glColor3f(1, 1, 1);
+        glScalef(0.5, 1, 0.7);
+        glTranslatef(0, 0.03, 0);
+        glutSolidCube(0.17);
+    glPopMatrix();
+    
+    glPushMatrix(); // glavni deo 2
+        glColor3f(1, 1, 1);
+        glScalef(0.5, 0.4, 0.70);
+        glTranslatef(0, -0.056, -0.04);
+        glutSolidCube(0.17);
+    glPopMatrix();
+    
+    glPushMatrix(); // leva noga
+        glColor3f(1, 0.4, 0);
+        glScalef(0.5, 1, 0.5);
+        glTranslatef(0.05, -0.07, -0.01);
+        glutSolidCube(0.03);
+    glPopMatrix();
+    
+    glPushMatrix(); // desna noga
+        glColor3f(1, 0.4, 0);
+        glScalef(0.5, 1, 0.5);
+        glTranslatef(-0.05, -0.07, -0.01);
+        glutSolidCube(0.03);
+    glPopMatrix();
+    
+    glPushMatrix(); // levo oko
+        glColor3f(0, 0, 0);
+        glTranslatef(0.043, 0.07, 0.02);
+        glutSolidCube(0.015);
+    glPopMatrix();
+    
+    glPushMatrix(); // desno oko
+        glColor3f(0, 0, 0);
+        glTranslatef(-0.043, 0.07, 0.02);
+        glutSolidCube(0.015);
+    glPopMatrix();
+    
+    glPushMatrix(); // gornji kljun
+        glColor3f(1, 0.4, 0);
+        glTranslatef(0, 0.065, 0.075);
+        glutSolidCube(0.03);
+    glPopMatrix();
+    
+    glPushMatrix(); // donji kljun
+        glColor3f(1, 0, 0);
+        glTranslatef(0, 0.04, 0.068);
+        glutSolidCube(0.02);
+    glPopMatrix();
+    
+    glPushMatrix(); // levo krilo
+        glColor3f(1, 1, 1);
+        glScalef(0.25, 0.2, 0.35);
+        glTranslatef(0.17, -0.12, -0.08);
+        glutSolidCube(0.17);
+    glPopMatrix();
+    
+    glPushMatrix(); // desno krilo
+        glColor3f(1, 1, 1);
+        glScalef(0.25, 0.2, 0.35);
+        glTranslatef(-0.17, -0.12, -0.08);
+        glutSolidCube(0.17);
+    glPopMatrix();
+    
+    glPushMatrix(); // kresta
+        glColor3f(1, 0, 0);
+        glScalef(0.3, 0.3, 1);
+        glTranslatef(0, 0.40, 0);
+        glutSolidCube(0.05);
+    glPopMatrix();
+}
+
+static void rotateChicken(char current_step){
+    
+    glRotatef(180, 0, 1, 0);
+    
+    if(current_step == 'l')
+        glRotatef(90, 0, 1, 0);
+    else if(current_step == 'r')
+        glRotatef(-90, 0, 1, 0);
+    else if(current_step == 'b')
+        glRotatef(180, 0, 1, 0);
 }
 
 
