b6e132849f660cf45bdb7936f7d4e396bdbc1ea7
AleksandarLisov <sasa.lisov@yahoo.com>
2019-12-23 23:06:06 +0100

    Dodata struktura i organizovan prostor u vidu polja po kojima se pile krece, implementirana provera kada pile moze da skoci na polje

--- CrossTheRoad/main.c
+++ CrossTheRoad/main.c
@@ -3,17 +3,24 @@
 #include <math.h>
 #include <GL/glut.h>
 #include <time.h>
+#include <stdbool.h>
 
 static float animation_ongoing = 0;
 static float animation_parameter = 0;
 
-const static float size = 0.2; // velicina pileta
+// koordinata prednje ivice pileta na pocetku z = 0.91125
+// koordinata zadnje ivice pileta na pocetku z = 1.08875
+// koordinata desne ivice pileta na pocetku x = 0.06375
+// koordinata leve ivice pileta na pocetku x = -0.06375
+
+const static float sizeZ = 0.1775; // duzina pileta po z osi je 0.1775
+const static float sizeX = 0.1275; // sirina pileta po x osi je 0.1275
 const static float vector = 0.2; // velicina za koju se pile krece
 static char side; // pravac i smer u kom se pomera pile (l || r || f || b)
 static char current_step = 0; // trenutni potez koji je ucinjen - treba nam zbog rotacije pileta kad se krece u stranu
 
-static float x_curr; // x koordinata pileta po ravni z0
-static float y_curr; // y koordinata pileta po ravni z0
+static int x_curr; // x koordinata pileta
+static int z_curr; // z koordinata pileta
 
 static int points; // trenutni broj poena (uvecava se za svako kretanje unapred)
 
@@ -30,8 +37,25 @@ static void drawChicken(); // funkcija za crtanje pileta
 static void rotateChicken(char current_step); // funkcija koja rotira pile kad pri promeni pravca ili smera kretanja
 static void drawTree(); // funkcija koja crta drvo
 static void drawCar(); // funkcija koja crta automobil
+static void initialize_fields(); // funkcija koja inicijalizuje strukturu za polja
+
+static void setCar(); // postavlja automobil na odredjeno polje i postavlja vrednost polja na zauzeto
+static void setTree(); // postavlja drvo na odredjeno polje i postavlja vrednost polja na zauzeto
+
+static bool isFree(int coordinate1, int coordinate2); // proverava da li je slobodno polje na koje pile zeli da skoci
 
 #define TIMER_ID 0
+
+typedef struct field_struct{
+    bool taken;
+    float x_coordinate[2];
+    float middle_of_X;
+    float z_coordinate[2];
+    float middle_of_Z;
+    char teren;
+}field_struct;
+
+field_struct field[20][15];
 
 int main(int argc, char** argv){
     
@@ -63,7 +87,6 @@ int main(int argc, char** argv){
     glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    
     initialize();
     
     glutMainLoop();
@@ -85,16 +108,12 @@ static void on_display(void){
     glColor3f(0, 0, 1);
     
     glPushMatrix();
-        glTranslatef(x_curr, 0, -y_curr);
+        glTranslatef(field[x_curr][z_curr].middle_of_X, 0, field[x_curr][z_curr].middle_of_Z);
         drawChicken();
     glPopMatrix();
     
-    glPushMatrix();
-        glTranslatef(1, 0, 0);
-        drawTree();
-    glPopMatrix();
-        
-    drawCar();
+    setCar();
+    setTree();
         
     glutSwapBuffers();
 }
@@ -141,38 +160,39 @@ static void on_keyboard_for_arrows(int key, int x, int y){
     ////////////////////////////////////////////////////////
 }
 
-static void jumpCheck(unsigned char m){ //////////////////////////////////////////////////////// kretanje pileta i granice kretanja
+static void jumpCheck(unsigned char m){
     
     switch(m){
         
         case 'l':
-            if (x_curr - vector >= -(1.5 - size / 2)){
-                x_curr -= vector;
+            if (z_curr > 0 && isFree(x_curr, z_curr - 1)){
+                z_curr --;
             }
             current_step = 'l';
             checkAndStartTimer();
             glutPostRedisplay();
             break;
         case 'r':
-            if (x_curr + vector <= 1.5 - size / 2){
-                x_curr += vector;
+            if (z_curr < 14 && isFree(x_curr, z_curr + 1)){
+                z_curr ++;
             }
             current_step = 'r';
             checkAndStartTimer();
             glutPostRedisplay();
             break;
         case 'f':
-            y_curr += vector;
+            if(isFree(x_curr + 1, z_curr)){
+                x_curr ++;
+                points ++;
+            }
             current_step = 'f';
             checkAndStartTimer();
             glutPostRedisplay();
-            points ++;
             printf("Points: %d\n", points);
             break;
         case 'b':
-            if (y_curr - vector >= -(1 - size / 2)){
-                y_curr -= vector;
-                points --;;
+            if (x_curr > 0 && isFree(x_curr - 1, z_curr)){ // jer je na pocetku koordinata donje ivice pileta jednaka 1.08875
+                x_curr --;
                 printf("Points: %d\n", points);
             }
             current_step = 'b';
@@ -186,8 +206,10 @@ static void jumpCheck(unsigned char m){ ////////////////////////////////////////
 
 static void initialize(void){
     
+    initialize_fields();
+    
     x_curr = 0;
-    y_curr = -1;
+    z_curr = 8;
     
     points = 0;
 }
@@ -342,24 +364,28 @@ static void drawCar(){
     glPushMatrix(); // zadnji desni tocak
         glColor3f(0.1, 0.1, 0.1);
         glTranslatef(-0.05, -0.025, 0.05);
+        glScalef(1, 1, 0.1);
         glutSolidSphere(0.025, 30, 2);
     glPopMatrix();
     
     glPushMatrix(); // zadnji levi tocak
         glColor3f(0.1, 0.1, 0.1);
         glTranslatef(-0.05, -0.025, -0.05);
+        glScalef(1, 1, 0.1);
         glutSolidSphere(0.025, 30, 2);
     glPopMatrix();
     
     glPushMatrix(); // prednji desni tocak
         glColor3f(0.1, 0.1, 0.1);
         glTranslatef(0.05, -0.025, 0.05);
+        glScalef(1, 1, 0.1);
         glutSolidSphere(0.025, 30, 2);
     glPopMatrix();
     
     glPushMatrix(); // prednji levi tocak
         glColor3f(0.1, 0.1, 0.1);
         glTranslatef(0.05, -0.025, -0.05);
+        glScalef(1, 1, 0.1);
         glutSolidSphere(0.025, 30, 2);
     glPopMatrix();
     
@@ -383,4 +409,64 @@ static void drawCar(){
         glTranslatef(0, 0.19, 0.3);
         glutSolidCube(0.09);
     glPopMatrix();
+}
+
+static void initialize_fields(){
+    
+    // rucno inicijalizujemo donje levo polje
+    field[0][0].x_coordinate[0] = -1.4 - sizeX / 2;
+    field[0][0].x_coordinate[1] = -1.4 + sizeX / 2;
+    field[0][0].z_coordinate[0] = 1 + sizeZ / 2;
+    field[0][0].z_coordinate[1] = 1 - sizeZ / 2;
+    
+    // popunjavamo koordinate za ostala polja u istoj koloni kao donje levo polje
+    for (int i = 1; i < 20; i++){
+        field[i][0].x_coordinate[0] = field[i-1][0].x_coordinate[0];
+        field[i][0].x_coordinate[1] = field[i-1][0].x_coordinate[1];
+        field[i][0].z_coordinate[0] = field[i-1][0].z_coordinate[0] - vector;
+        field[i][0].z_coordinate[1] = field[i-1][0].z_coordinate[1] - vector;
+    }
+    
+    for (int i = 0; i < 20; i++){
+        for (int j = 1; j < 15; j++){
+            field[i][j].x_coordinate[0] = field[i][j-1].x_coordinate[0] + vector;
+            field[i][j].x_coordinate[1] = field[i][j-1].x_coordinate[1] + vector;
+            field[i][j].z_coordinate[0] = field[i][j-1].z_coordinate[0];
+            field[i][j].z_coordinate[1] = field[i][j-1].z_coordinate[1];
+        }
+    }
+    
+    for (int i = 0; i < 20; i++){
+        for (int j = 0; j < 15; j++){
+            field[i][j].middle_of_X = field[i][j].x_coordinate[0] + (field[i][j].x_coordinate[1] - field[i][j].x_coordinate[0]) / 2;
+            field[i][j].middle_of_Z = field[i][j].z_coordinate[0] + (field[i][j].z_coordinate[1] - field[i][j].z_coordinate[0]) / 2;
+        }
+    }
+}
+
+static void setCar(){
+    
+    glPushMatrix();
+        glTranslatef(field[5][6].middle_of_X, 0, field[5][6].middle_of_Z);
+        drawCar();
+    glPopMatrix();
+    
+    field[5][6].taken = 1;
+}
+
+static void setTree(){
+    
+    glPushMatrix();
+        glTranslatef(field[5][8].middle_of_X, 0, field[5][8].middle_of_Z);
+        drawTree();
+    glPopMatrix();
+    
+    field[5][8].taken = 1;
+}
+
+static bool isFree(int coordinate1, int coordinate2){
+    if(field[coordinate1][coordinate2].taken == 1)
+        return 0;
+    else
+        return 1;
 }
